f4b32ddc14cf061a3fdcecbacfb8e1966cca7b01
asterodeia <d_wang_890227@outlook.com>
2026-03-12 10:04:47 +0800

    finish algorithm3-8

--- exercises/algorithm/algorithm3.rs
+++ exercises/algorithm/algorithm3.rs
@@ -1,12 +1,29 @@
 /*
-	sort
-	This problem requires you to implement a sorting algorithm
-	you can use bubble sorting, insertion sorting, heap sorting, etc.
+    sort
+    This problem requires you to implement a sorting algorithm
+    you can use bubble sorting, insertion sorting, heap sorting, etc.
 */
-// I AM NOT DONE
 
-fn sort<T>(array: &mut [T]){
-	//TODO
+fn sort<T>(array: &mut [T])
+where
+    T: std::cmp::PartialOrd + Clone,
+{
+    //TODO
+    if array.is_empty() {
+        return;
+    }
+
+    for idx in 1..array.len() {
+        let target = array[idx].clone();
+        let mut insert_loc = idx;
+
+        while insert_loc > 0 && target < array[insert_loc - 1] {
+            array[insert_loc] = array[insert_loc - 1].clone();
+            insert_loc -= 1;
+        }
+
+        array[insert_loc] = target;
+    }
 }
 #[cfg(test)]
 mod tests {
@@ -18,13 +35,13 @@ mod tests {
         sort(&mut vec);
         assert_eq!(vec, vec![19, 37, 46, 57, 64, 73, 75, 91]);
     }
-	#[test]
+    #[test]
     fn test_sort_2() {
         let mut vec = vec![1];
         sort(&mut vec);
         assert_eq!(vec, vec![1]);
     }
-	#[test]
+    #[test]
     fn test_sort_3() {
         let mut vec = vec![99, 88, 77, 66, 55, 44, 33, 22, 11];
         sort(&mut vec);
--- exercises/algorithm/algorithm8.rs
+++ exercises/algorithm/algorithm8.rs
@@ -1,8 +1,7 @@
 /*
-	queue
-	This question requires you to use queues to implement the functionality of the stac
+    queue
+    This question requires you to use queues to implement the functionality of the stac
 */
-// I AM NOT DONE
 
 #[derive(Debug)]
 pub struct Queue<T> {
@@ -52,41 +51,56 @@ impl<T> Default for Queue<T> {
     }
 }
 
-pub struct myStack<T>
-{
-	//TODO
-	q1:Queue<T>,
-	q2:Queue<T>
+pub struct myStack<T> {
+    //TODO
+    q1: Queue<T>,
+    q2: Queue<T>,
 }
 impl<T> myStack<T> {
     pub fn new() -> Self {
         Self {
-			//TODO
-			q1:Queue::<T>::new(),
-			q2:Queue::<T>::new()
+            //TODO
+            q1: Queue::<T>::new(),
+            q2: Queue::<T>::new(),
         }
     }
     pub fn push(&mut self, elem: T) {
         //TODO
+        match (self.q1.size(), self.q2.size()) {
+            (_, 0) => self.q1.enqueue(elem),
+            _ => self.q2.enqueue(elem),
+        }
     }
     pub fn pop(&mut self) -> Result<T, &str> {
-        //TODO
-		Err("Stack is empty")
+        match (self.q1.size(), self.q2.size()) {
+            (0, 0) => Err("Stack is empty"),
+            (s1, 0) => {
+                for _ in 0..s1 - 1 {
+                    self.q2.enqueue(self.q1.dequeue().unwrap());
+                }
+                Ok(self.q1.dequeue().unwrap())
+            }
+            (_, s2) => {
+                for _ in 0..s2 - 1 {
+                    self.q1.enqueue(self.q2.dequeue().unwrap());
+                }
+                Ok(self.q2.dequeue().unwrap())
+            }
+        }
     }
     pub fn is_empty(&self) -> bool {
-		//TODO
-        true
+        matches!((self.q1.size(), self.q2.size()), (0, 0))
     }
 }
 
 #[cfg(test)]
 mod tests {
-	use super::*;
-	
-	#[test]
-	fn test_queue(){
-		let mut s = myStack::<i32>::new();
-		assert_eq!(s.pop(), Err("Stack is empty"));
+    use super::*;
+
+    #[test]
+    fn test_queue() {
+        let mut s = myStack::<i32>::new();
+        assert_eq!(s.pop(), Err("Stack is empty"));
         s.push(1);
         s.push(2);
         s.push(3);
@@ -100,5 +114,6 @@ mod tests {
         assert_eq!(s.pop(), Ok(1));
         assert_eq!(s.pop(), Err("Stack is empty"));
         assert_eq!(s.is_empty(), true);
-	}
+    }
 }
+
